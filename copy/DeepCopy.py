cd6ebadeb5fa30a99b22da683404524f3f64d64b
Shakir Gooty <shakir544@gmail.com>
2018-04-04 00:26:36 -0400

    update DeepCopy with correct example

--- copy/DeepCopy.py
+++ copy/DeepCopy.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python
 
 # Python code demostrate copy functions
-
 import copy
 
+
 # custom function to print the elements in list
-
-
 def print_list(list):
     for item in list:
         print(item)
 
 
 # initialize a list
-src = [1,2,3,4,5]
+src = [1,2,3,[4,5]]
 
 # using deep copy to copy
 tar = copy.deepcopy(src)
 
-# append a number in tar list
-tar.append(6)
+# modify the tar list
+tar[3][1] = 544
 
 # print both src and tar list
 print("Print src list ")
