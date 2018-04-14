afdef391cef4bda50597721a79d13f7c0fed164b
Shakir Gooty <shakir544@gmail.com>
2018-04-14 12:38:14 -0400

    update To check type of file in a directory

--- os/filechecker.py
+++ os/filechecker.py
@@ -1,26 +1,48 @@
 # Python script to check the number of files, find duplicates, sizes of the files
 import os
 import sys
-import shutil
 
 
-# get the details of folder
-if not len(sys.argv) > 1:
-    print("Not enough number of args to filechecker")
-    sys.exit(1)
-
-sourcedir = sys.argv[1]
-
-if os.path.isdir(sourcedir):
-    # list the files in the directory
-    files = os.listdir(sourcedir)
-
-else :
-    print(sourcedir + " not exists. Provide a directory to check the details")
-    sys.exit(1)
+def filecount(dir_location):
+    files = os.listdir(dir_location)
+    sym_links_files = []
+    regular_files = []
+    for file in files:
+        name = os.path.abspath(os.path.join(dir_location,file))
+        if os.path.islink(name):
+            sym_links_files.append(name)
+        else :
+            regular_files.append(name)
+    display(sym_links_files,"symlinks")
+    display(regular_files,"regular")
 
 
-def display(files):
+def decorate(fun):
+    def decorate_display(files, filetype):
+        print("-------------------")
+        print(filetype)
+        fun(files,filetype)
+        print("-------------------")
+    return decorate_display
+
+
+@decorate
+def display(files,filetype):
     for count, file in enumerate(files):
-        print("{0} - {1}".format(count,file))
+        print("{0} - {1} - {2}".format(count,filetype,file))
 
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Not enough number of args to filechecker")
+        print("Usage: filechecker.py <directory_name>")
+        sys.exit(1)
+
+    location = sys.argv[1]
+
+    if os.path.isdir(location):
+        print(location + " is a directory and exists")
+        filecount(location)
+    else:
+        print("Input argument is invalid. Target Directory location is not directory")
+        sys.exit(1)
