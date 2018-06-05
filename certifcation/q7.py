da04c7581277b634756a163b881be09354c3b108
Shakir Gooty <shakir544@gmail.com>
2018-06-05 18:10:43 -0400

    Add Sample practise questions

---
+++ certifcation/q7.py
@@ -0,0 +1,11 @@
+# Practise question 7
+
+a = "testing"
+b = "testing"
+
+'''
+which operator should you use to check both are same ?
+
+'''
+
+print(a is b)
---
+++ certifcation/q8.py
@@ -0,0 +1,11 @@
+# practise question 8
+
+a = '''
+which of the following respects the whites spaces 
+  example below 
+'''
+
+b = " testing " \
+    "if this one repects whitespaces"
+print(a)
+
