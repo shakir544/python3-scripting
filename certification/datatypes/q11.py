56b69c9ee50256f0cb0fc84626a0b2d087118990
Shakir Gooty <shakir544@gmail.com>
2018-09-02 09:02:54 -0400

    Q11 Type of data type when using input() in python

---
+++ certification/datatypes/q11.py
@@ -0,0 +1,16 @@
+# Question 11
+# Perform opertaions using data types and operators
+
+'''
+count = input("Enter the number ")
+add a new line
+print(new_count)
+
+new_count = ?
+
+'''
+
+count = input("Enter the number ")
+new_count = int(count) + 5
+print(new_count)
+
