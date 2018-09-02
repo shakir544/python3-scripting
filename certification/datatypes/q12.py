9df13da26100c7d47059e3555dab5300d79daf5f
Shakir Gooty <shakir544@gmail.com>
2018-09-02 09:08:49 -0400

    Q12 : Check the datatype using type()

---
+++ certification/datatypes/q12.py
@@ -0,0 +1,21 @@
+# Question 12
+# Perform operations using data types and operators
+
+"""
+Determine the datatype of the following program
+print("Determine the dataype")
+#---add the line
+print(type(my_var))
+print("Wrapping up of program")
+
+my_var = '45'
+my_var = 2 + 4j
+my_var = 47.0
+my_var = 2**2
+"""
+
+print("Determine the dataype")
+my_var = 2**2
+print(type(my_var))
+print("Wrapping up of program")
+
